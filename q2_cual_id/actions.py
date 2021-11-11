1b4085865b6ee49b157a588c33cbc4fb3723f698
antonhibl <antonhibl11@gmail.com>
2021-11-10 18:32:22 -0700

    Method calls and type definitions

---
+++ q2_cual_id/actions.py
@@ -0,0 +1,33 @@
+# ----------------------------------------------------------------------
+# Copyright (c) 2016-2021, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed w/ this software.
+# ----------------------------------------------------------------------
+
+import cualid
+from cualid.label import get_barcodes
+from cualid.mint import create_ids
+from cualid.fix import fix_ids, format_output,
+
+def generate_id(
+        
+        ):
+    # initialize local variables and data
+    # utilize create ids command
+    # parse output into a qiime artifact
+    # return qiime artifact
+
+def generate_barcodes():
+    # initialize local variables
+    # utilize get barcodes command
+    # create a visualizer that allows for storing & viewing of the barcodes
+    # Return the visualizer artifact
+
+def check_ids():
+    # initialize local variables and data, possible flags
+    # extract id data from the provided artifact
+    # use the fix_ids command to manage the id metadata from an artifact
+    # re-parse the fixed ID metadata into the artifact
+    # return the re-generated artifact
--- setup.py
+++ setup.py
@@ -1,31 +1,33 @@
-# ----------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 # Copyright (c) 2016-2021, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file LICENSE, distributed with this software.
-# ----------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 
 from setuptools import setup, find_packages
 
 import versioneer
 
-
 setup(
     name="q2-cual-id",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    url="https://qiime2.org",
-    license="BSD-3-Clause",
     packages=find_packages(),
-    author="John Chase and Evan Bolyen",
-    author_email="ebolyen@gmail.com",
-    description="Create and manage sample identifiers in comparative -omics datasets.",
-    package_data={
-        'q2_cual_id': ['citations.bib'],
-    },
+    author="Anton Hibl",
+    author_email="antonhibl11@gmail.com",
+    description="Global 'Cual' ID toolkit plugin for QIIME2.",
+    license='BSD-3-Clause',
+    url="https://qiime2.org",
     entry_points={
-        "qiime2.plugins":
-        ["q2-cual-id=q2_cual_id.plugin_setup:plugin"]
+        'qiime2.plugins':
+        ['q2-cual-id=q2_cual_id.plugin_setup:plugin']
+    },
+    package_data={
+        'q2_cual_id': [
+            'citations.bib',
+        ]
     },
     zip_safe=False,
+)
